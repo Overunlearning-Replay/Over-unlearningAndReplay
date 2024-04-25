9384bd85dc25646250c11174d7c92e727fdf8b01
Overunlearning-Replay <167877416+Overunlearning-Replay@users.noreply.github.com>
2024-04-25 21:07:13 +0800

    Update feature_visualization_metric_main.py

--- feature_visualization_metric_main.py
+++ feature_visualization_metric_main.py
@@ -85,23 +85,6 @@ parser.add_argument(
     type=str,
     default="baseline",
     nargs="?",
-    # choices=[
-    #     "baseline",
-    #     "retrain",
-    #     "finetune",
-    #     "blindspot",
-    #     "amnesiac",
-    #     "UNSIR",
-    #     "NTK",
-    #     "ssd_tuning",
-    #     "FisherForgetting",
-    #     'Wfisher',
-    #     'FT_prune',
-    #     'negative_grad',
-    #     'negative_grad_relabel',
-    #     'negative_grad_with_prune',
-    #     "blindspot_with_prune"
-    # ],
     help="select unlearning method from choice set",
 )
 parser.add_argument(
@@ -140,7 +123,7 @@ args = parser.parse_args()
 def extract_features(dataloader, model_temp, device):
     features = []
     labels = []
-    model_temp.eval()  # 设置为评估模式
+    model_temp.eval()
     with torch.no_grad():
         for i, data in enumerate(dataloader, 0):
             inputs, _, label = data
@@ -170,40 +153,17 @@ def tsne_visualization(train_loader, model, forget_class, device, name, seed=42)
     return Q, reduced_features, labels
 
 def kl_divergence(P, Q):
-    """计算两个矩阵之间的KL散度，确保没有零值问题"""
-    # 将所有的零值替换为很小的正数，防止计算log(0)
     P_safe = np.where(P == 0, np.finfo(float).eps, P)
     Q_safe = np.where(Q == 0, np.finfo(float).eps, Q)
 
     return np.sum(P_safe * np.log(P_safe / Q_safe))
 
 def feature_visualization_metric(unlearned_model, device):
-    # 特征提取
-    # features, labels = extract_features(full_train_dl, model)
-    # # 使用t-SNE进行降维
-    # tsne = TSNE(n_components=2, random_state=42)
-    # reduced_features = tsne.fit_transform(features)
-    #
-    # plt.figure(figsize=(10, 10))
-    # for i in range(19):
-    #     indices = labels == i
-    #     plt.scatter(reduced_features[indices, 0], reduced_features[indices, 1], label=str(i), alpha=0.5)
-    # plt.legend()
-    # plt.savefig(visual_path + f"/{args.method}_feature_visualization_pretrain.png")
-
-    # tsne_visualization(full_train_dl, model, int(args.forget_class), device, name='pretrained_model')
-
     return tsne_visualization(full_train_dl, unlearned_model,  int(args.forget_class), device, name='unlearned_model', seed=42)
-    # Q2 = tsne_visualization(full_train_dl, unlearned_model, int(args.forget_class), device, name='unlearned_model', seed=41)
-
-
-    # 使用t-SNE进行降维
-    # tsne_visualization(train_dl_w_ood, unlearned_model, int(args.forget_class), device, name='unlearned_model_w_ood')
-
+    
 def compute_centroid(points):
     return np.mean(points, axis=0)
 
-# 假设Y是低维空间的坐标矩阵，labels是对应的标签数组
 def class_to_overall_relationship(Y, labels):
     unique_labels = np.unique(labels)
     overall_centroid = compute_centroid(Y)
@@ -219,13 +179,11 @@ def calculate_class_variances(Y, labels, unlearned_label=4):
     overall_centroid = np.mean(Y, axis=0)
     class_centroids = np.array([np.mean(Y[labels == label], axis=0) for label in unique_labels])
 
-    # 类内方差：计算每个类内所有点与该类质心的距离平方的平均值
     intra_class_variances = {
         label: np.mean(np.sum((Y[labels == label] - class_centroids[i]) ** 2, axis=1))
         for i, label in enumerate(unique_labels)
     }
 
-    # 类间方差：计算类质心与整体质心间的距离平方
     inter_class_variance = np.sum((class_centroids - overall_centroid) ** 2, axis=1).mean()
 
     return intra_class_variances, inter_class_variance
@@ -244,12 +202,12 @@ from scipy.stats import gaussian_kde
 def estimate_overlap(Y, labels):
     unique_labels = np.unique(labels)
     kdes = {label: gaussian_kde(Y[labels == label].T) for label in unique_labels}
-    grid_points = np.linspace(np.min(Y), np.max(Y), 100)  # 根据数据调整
+    grid_points = np.linspace(np.min(Y), np.max(Y), 100)
     grid_mesh = np.meshgrid(grid_points, grid_points)
     grid_coords = np.vstack([grid.ravel() for grid in grid_mesh])
 
     densities = np.array([kde(grid_coords) for kde in kdes.values()])
-    overlap = np.min(densities, axis=0).sum()  # 取最小密度求和作为重叠度量
+    overlap = np.min(densities, axis=0).sum()
     return overlap
 
 import numpy as np
@@ -258,28 +216,22 @@ from scipy.stats import gaussian_kde
 import matplotlib.pyplot as plt
 
 def calculate_overlap(Y, labels, target_label, save_path):
-    # 第四类数据和其他类数据
     target_data = Y[labels == target_label]
     other_data = Y[labels != target_label]
 
-    # 使用高斯核密度估计
     target_kde = gaussian_kde(target_data.T)
     other_kde = gaussian_kde(other_data.T)
 
-    # 定义评估网格
     x_min, x_max = Y[:, 0].min() - 1, Y[:, 0].max() + 1
     y_min, y_max = Y[:, 1].min() - 1, Y[:, 1].max() + 1
     xx, yy = np.mgrid[x_min:x_max:200j, y_min:y_max:200j]
     positions = np.vstack([xx.ravel(), yy.ravel()])
 
-    # 计算密度值
     target_density = target_kde(positions).reshape(xx.shape)
     other_density = other_kde(positions).reshape(xx.shape)
 
-    # 计算重叠区域
     overlap = np.minimum(target_density, other_density)
 
-    # 可视化
     plt.figure(figsize=(4.1, 3.5))
     cax=plt.imshow(np.rot90(overlap), cmap=plt.cm.gist_earth_r, extent=[x_min, x_max, y_min, y_max])
     # plt.title('Overlap Area between Class 4 and Others')
@@ -288,12 +240,34 @@ def calculate_overlap(Y, labels, target_label, save_path):
     # plt.colorbar()
     # plt.show()
     formatter = ScalarFormatter(useMathText=True)
-    formatter.set_powerlimits((0, 0))  # 设置科学计数法的阈值
+    formatter.set_powerlimits((0, 0))
     plt.colorbar(cax, format=formatter)
     plt.savefig(save_path + f"/kde_overlap.pdf", bbox_inches='tight', format='pdf')
 
-    # 返回重叠区域的量化值
     return overlap.sum()
+
+def plot_tsne_visualization_results(reduced_features, forget_class=4, name='blindspot'):
+    retain_classes = list(set(range(20)) - set(config.ood_classes))
+    # print("retain_classes", retain_classes)
+    index_forget_class = retain_classes.index(forget_class)
+    plt.figure(figsize=(5.1, 4.5))
+    colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#9467bd",
+              "#d62728", "#8c564b", "#e377c2", "#7f7f7f",
+              "#bcbd22", "#17becf", "#65c2a4", "#89a6af",
+              "#df7976", "#e4d1dd", "#d1e0ab"]
+    for i in list(set(range(15)) - set([index_forget_class])):
+        indices = labels == i
+        plt.scatter(reduced_features[indices, 0], reduced_features[indices, 1], color=colors[i],
+                    label=str(retain_classes[i]), alpha=0.5)
+
+    for i in set([index_forget_class]):
+        indices = labels == i
+        plt.scatter(reduced_features[indices, 0], reduced_features[indices, 1], color=colors[i],
+                    label=str(retain_classes[i]), alpha=0.5)
+
+    plt.legend(loc=4)
+    plt.savefig(visual_path + f"/tsne_{name}.pdf", bbox_inches='tight', format='pdf')
+
 
 from sklearn.metrics import silhouette_score
 
@@ -318,7 +292,6 @@ if __name__ == '__main__':
                                    "{net}-{dataset}-{classes}".format(net=args.net, dataset=args.dataset,
                                                                       classes=args.classes),
                                    "{task}".format(task="unlearning"),
-                                   # "{unlearning_method}".format(unlearning_method=args.method))
                                    "{unlearning_method}-{para1}-{para2}".format(unlearning_method=args.method,
                                                                                 para1=args.para1, para2=args.para2))
 
@@ -328,7 +301,6 @@ if __name__ == '__main__':
                                        "{net}-{dataset}-{classes}".format(net=args.net, dataset=args.dataset,
                                                                           classes=args.classes),
                                        "{task}".format(task="unlearning"),
-                                       # "{unlearning_method}".format(unlearning_method=args.method))
                                        "{unlearning_method}-{para1}-{para2}".format(unlearning_method=args.method,
                                                                                     para1=args.para1,
                                                                                     para2=args.para2))
@@ -339,7 +311,6 @@ if __name__ == '__main__':
                                    "{net}-{dataset}-{classes}".format(net=args.net, dataset=args.dataset,
                                                                       classes=args.classes),
                                    "{task}".format(task="unlearning"),
-                                   # "{unlearning_method}".format(unlearning_method=args.method))
                                    "{unlearning_method}".format(unlearning_method=args.method))
 
         checkpoint_path = os.path.join(config.CHECKPOINT_PATH,
@@ -348,7 +319,6 @@ if __name__ == '__main__':
                                        "{net}-{dataset}-{classes}".format(net=args.net, dataset=args.dataset,
                                                                           classes=args.classes),
                                        "{task}".format(task="unlearning"),
-                                       # "{unlearning_method}".format(unlearning_method=args.method))
                                        "{unlearning_method}".format(unlearning_method=args.method))
 
     print("#####", checkpoint_path)
@@ -358,8 +328,6 @@ if __name__ == '__main__':
     # For celebritiy faces
     root = "105_classes_pins_dataset" if args.dataset == "PinsFaceRecognition" else "./data"
 
-    # Scale for ViT (faster training, better performance)
-    # Scale for ViT (faster training, better performance)
     img_size = 224 if args.net == "ViT" else 32
 
     trainset = getattr(datasets, args.dataset)(root=root, download=True, train=True, unlearning=True,
@@ -377,7 +345,6 @@ if __name__ == '__main__':
                                                                    int(args.forget_class), config.ood_classes)
 
     forget_train, forget_valid = classwise_train[int(args.forget_class)], classwise_test[int(args.forget_class)]
-    # counterpart_train, counterpart_valid = classwise_train[0], classwise_test[0]
 
     forget_valid_dl = DataLoader(forget_valid, batch_size)
     retain_valid_dl = DataLoader(retain_valid, batch_size)
@@ -387,8 +354,6 @@ if __name__ == '__main__':
     full_train_dl = DataLoader(ConcatDataset((retain_train_dl.dataset, forget_train_dl.dataset)),
                                batch_size=batch_size, )
 
-    # print("forget_train_dl", len(forget_train_dl.dataset))
-    # print("full_train_dl", len(ConcatDataset((retain_train_dl.dataset, forget_train_dl.dataset))))
 
     full_valid_dl = DataLoader(ConcatDataset((retain_valid_dl.dataset, forget_valid_dl.dataset)),
                                batch_size=batch_size, )
@@ -396,16 +361,16 @@ if __name__ == '__main__':
     ood_valid_ds = {}
     ood_train_ds = {}
     ood_valid_dl = []
-    ood_train_dl = []  # 存不同类别的分布外数据的dataloader
+    ood_train_dl = []
     for cls in config.ood_classes:
         ood_valid_ds[cls] = []
         ood_train_ds[cls] = []
 
         for img, label, clabel in classwise_test[cls]:
-            ood_valid_ds[cls].append((img, label, int(args.forget_class)))  # 分布外的数据默认为遗忘类
+            ood_valid_ds[cls].append((img, label, int(args.forget_class)))
 
         for img, label, clabel in classwise_train[cls]:
-            ood_train_ds[cls].append((img, label, int(args.forget_class)))  # 分布外的数据默认为遗忘类
+            ood_train_ds[cls].append((img, label, int(args.forget_class)))
 
         ood_valid_dl.append(DataLoader(ood_valid_ds[cls], batch_size))
         ood_train_dl.append(DataLoader(ood_train_ds[cls], batch_size))
@@ -426,17 +391,8 @@ if __name__ == '__main__':
         unlearned_net = unlearned_net.cuda()
 
     kwargs = {
-        # "model": net,
         "unlearned_model": unlearned_net,
-        # "retain_train_dl": retain_train_dl,
-        # "forget_train_dl": forget_train_dl,
-        # "full_train_dl": full_train_dl,
-        # "valid_dl": full_valid_dl,#validloader,
-        # "forget_class": forget_class,
-        # "num_classes": args.classes,
-        # "dataset_name": args.dataset,
         "device": "cuda" if args.gpu else "cpu",
-        # "model_name": args.net,
     }
 
     if args.para1:
@@ -457,24 +413,21 @@ if __name__ == '__main__':
                                    "{unlearning_method}".format(unlearning_method=args.method))
 
     print("#####", visual_path)
-    # if not os.path.exists(visual_path):
-    #     os.makedirs(visual_path)
-    #
-    # Q, reduced_features, labels = feature_visualization_metric(**kwargs)
-    # np.save(osp.join(q_save_path, 'q.npy'), Q)
-    # np.save(osp.join(q_save_path, 'reduced_features.npy'), reduced_features)
-    # np.save(osp.join(q_save_path, 'labels.npy'), labels)
+    if not os.path.exists(visual_path):
+       os.makedirs(visual_path)
+    
+    Q, reduced_features, labels = feature_visualization_metric(**kwargs)
+    np.save(osp.join(q_save_path, 'q.npy'), Q)
+    np.save(osp.join(q_save_path, 'reduced_features.npy'), reduced_features)
+    np.save(osp.join(q_save_path, 'labels.npy'), labels)
+    
+    plot_tsne_visualization_results(reduced_features, name=args.method)
 
-    # # Q_test = np.load(osp.join(q_save_path, 'q.npy'))
     reduced_features = np.load(osp.join(q_save_path, 'reduced_features.npy'))
     labels = np.load(osp.join(q_save_path, 'labels.npy'))
 
-    # print("{unlearning_method}-{para1}-{para2}".format(unlearning_method=args.method,
-    #                                                                         para1=args.para1, para2=args.para2))
-
-    # 假设Y是低维空间的坐标矩阵，labels是对应的标签数组
     intra_variances, inter_variance = calculate_class_variances(reduced_features, labels,
-                                                                unlearned_label=4)  # unlearned_label=4
+                                                                unlearned_label=4)
     print("Intra-class Variances:", intra_variances[4])
 
 
