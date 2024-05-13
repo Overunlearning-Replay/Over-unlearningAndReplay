5437db97385c99ff0d08bd6a9ddbe23b7b9404d6
Overunlearning-Replay <167877416+Overunlearning-Replay@users.noreply.github.com>
2024-05-13 09:27:44 +0800

    Add files via upload

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -36,7 +36,12 @@ transform_test = [
 ]
 
 transform_gtsrb = transforms.Compose([
-    transforms.Resize((64, 64)),#224
+    transforms.Resize((32, 32)),#224 64
+    transforms.ToTensor(),
+])
+
+transform_imagenet = transforms.Compose([
+    transforms.Resize((32, 32)),#224 64
     transforms.ToTensor(),
 ])
 
@@ -133,6 +138,7 @@ class Cifar20(CIFAR100):
             assert coarse_y != None
         return x, y, coarse_y
 
+
 class Cifar10(CIFAR10):
     def __init__(self, root, train, unlearning, download, img_size=32):
         if train:
@@ -189,6 +195,7 @@ class TriggerHandler(object):
     def put_trigger(self, img):
         img.paste(self.trigger_img, (self.img_width - self.trigger_size, self.img_height - self.trigger_size))
         return img
+
 
 class Cifar10Poison(CIFAR10):
     def __init__(self, args, root, train, unlearning, download, img_size=32):
@@ -281,3 +288,47 @@ class GTSRB(Dataset):
         image = self.transforms(image)
         label = self.labels[index]
         return image, label
+
+
+import pickle
+
+class Imagenet64(Dataset):
+    def __init__(self, root, train=False):
+        self.data = []
+        self.labels = []
+        self.transform = transform_imagenet
+        self.train = train
+        self.load_data(root)
+
+    def load_data(self, root):
+        # 加载所有批次文件
+        if self.train:
+            for i in range(1, 11):  # 从1到10
+                file_path = f"{root}/Imagenet64_train/train_data_batch_{i}"
+                with open(file_path, 'rb') as file:
+                    batch_data = pickle.load(file, encoding='bytes')
+                # ImageNet64 数据通常是 N x 3072 的数组，需要重新塑形为 64x64x3
+                images = batch_data['data'].reshape((-1, 3, 64, 64)).transpose((0, 2, 3, 1))
+                self.data.extend(images)
+                self.labels.extend(batch_data['labels'])
+        else:
+            file_path = f"{root}/Imagenet64_val/val_data"
+            with open(file_path, 'rb') as file:
+                batch_data = pickle.load(file, encoding='bytes')
+            # ImageNet64 数据通常是 N x 3072 的数组，需要重新塑形为 64x64x3
+            images = batch_data['data'].reshape((-1, 3, 64, 64)).transpose((0, 2, 3, 1))
+            self.data.extend(images)
+            self.labels.extend(batch_data['labels'])
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img = self.data[idx]
+        label = self.labels[idx] - 1  # 标签调整为从0开始
+        img = Image.fromarray(img)
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, label
